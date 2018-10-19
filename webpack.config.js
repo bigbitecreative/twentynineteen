e81c74ee8e060acd8d23654f676aa7bbfd9cb043
Scott Blackburn <scott@bigbitecreative.com>
2018-10-19 17:01:11 +0100

    basic webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,59 @@
-//webpack.config.js
+const path = require('path');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const extractSass = new ExtractTextPlugin({
+    filename: "style.css",
+});
 
-var webpack = require('webpack')
-
-module.exports = function(env) {
-    return {
-        entry: "./js/app.js",
-        output: {
-            path: __dirname + "/dist",
-            filename: "bundle.js"
-        },
+module.exports = {
+    entry: './index.js',
+    plugins: [
+        // Adding our UglifyJS plugin
+        new UglifyJSPlugin(),
+        extractSass
+    ],
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, '')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['babel-preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                loader: 'style-loader',
+            },
+            {
+                test: /\.css$/,
+                loader: 'css-loader',
+                options: {
+                minimize: true
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: extractSass.extract({
+                    use: [{
+                        loader: "css-loader",
+                        options: {
+                            minimize: true
+                        }
+                    }, {
+                        loader: "sass-loader"
+                    }],
+                    // use style-loader in development
+                    fallback: "style-loader"
+                })
+            },
+        ]
     }
-}
+};
