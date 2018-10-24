2c313f94fbc8ee8b28c652754e1eda2137e4a561
Scott Blackburn <scott@bigbitecreative.com>
2018-10-24 18:02:06 +0100

    Add autoprefixer working

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,11 @@
-module.exports = {
-    plugins: {
-        autoprefixer: {}
-    }
-};
+const autoprefixer = require('autoprefixer');
+
+module.exports = (context) => {
+  const env = context.webpack.options.mode;
+
+  const plugins = [
+    autoprefixer(),
+  ];
+
+  return { plugins }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 //const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const webpack = require("webpack");
+const autoprefixer = require("autoprefixer");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin-with-rtl");
 const WebpackRTLPlugin = require("webpack-rtl-plugin");
 
@@ -17,56 +19,48 @@ module.exports = {
       	rtlGlobalVar: 'pageDir',
 			}),
 			new WebpackRTLPlugin(),
+			new webpack.LoaderOptionsPlugin({
+				options: {
+					postcss: [
+						autoprefixer()
+					]
+				}
+			})
     ],
     output: {
-        filename: 'bundle.js',
         path: path.resolve(__dirname, '')
     },
     module: {
         rules: [
-            {
-                test: /\.js$/,
-                exclude: /(node_modules|bower_components)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['babel-preset-env']
-                    }
-                }
-            },
-      {
-        test: /\.(sa|sc|c)ss$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          {
-            loader: 'css-loader',
-            options: {
-              sourceMap: true,
-							minimize: false,
-            }
-          },
-          {
-            loader: 'postcss-loader',
-            options: {
-              sourceMap: true
-            }
-          },
-         {
-            loader: 'resolve-url-loader',
-            options: {
-              debug: true,
-              sourceMap: true,
-            }
-          },
-          {
-            loader: 'sass-loader',
-            options: {
-              includePaths: [require('node-normalize-scss').includePaths],
-              sourceMap: true,
-            }
-          }
-        ]
-      },
+				{
+					test: /\.(sa|sc|c)ss$/,
+					use: [
+						MiniCssExtractPlugin.loader,
+						{
+							loader: 'css-loader',
+							options: {
+							}
+						},
+						{
+							loader: 'postcss-loader',
+							options: {
+							}
+						},
+						{
+							loader: 'resolve-url-loader',
+							options: {
+								debug: true,
+								sourceMap: true,
+							}
+						},
+						{
+							loader: 'sass-loader',
+							options: {
+								sourceMap: true,
+							}
+						}
+					]
+				},
       ]
     }
 };
